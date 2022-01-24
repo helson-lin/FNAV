ef7302bd0d75d0f1d4dd8877cd179514128535d6
hejianglin <hejianglin@uino.com>
2022-01-24 10:23:02 +0800

    fix: ci运行问题

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -55,58 +55,6 @@ export default {
       background-size: cover;
       background-blend-mode: overlay;
     }
-    @property --xPoint {
-      syntax: '<length>';
-      inherits: false;
-      initial-value: 400px;
-    }
-    @property --yPoint {
-      syntax: '<length>';
-      inherits: false;
-      initial-value: 300px;
-    }
-
-    .wrap {
-      position: relative;
-      margin: auto;
-      width: 1000px;
-      height: 600px;
-      background:
-        conic-gradient(
-            from -45deg at var(--xPoint) var(--yPoint),
-            hsla(170deg, 100%, 70%, .7),
-            transparent 50%,
-            hsla(219deg, 90%, 80%, .5) 100%),
-        linear-gradient(-45deg, #060d5e, #002268);
-      animation: pointMove 2.5s infinite alternate linear;
-    }
-
-    .shadow {
-      position: absolute;
-      top: -300px;
-      left: -330px;
-      width: 430px;
-      height: 300px;
-      background: #fff;
-      transform-origin: 100% 100%;
-      transform: rotate(225deg);
-      clip-path: polygon(-100% 100%, 200% 100%, 200% 500%, -100% 500%);
-      animation: scale 2.5s infinite alternate linear;
-    }
-
-    @keyframes scale {
-      50%,
-      100% {
-        transform: rotate(225deg) scale(0);
-      }
-    }
-
-    @keyframes pointMove {
-      100% {
-        --xPoint: 100px;
-        --yPoint: 0;
-      }
-    }
 
   }
 }
